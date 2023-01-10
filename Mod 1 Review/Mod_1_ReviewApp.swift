da50312ff11d400ed6a2c38a0ccaf4fe380b1f03
Alistair Fraser <afraser@lcs.on.ca>
2023-01-10 10:58:55 -0500

    Initial Commit

---
+++ Mod 1 Review/Mod_1_ReviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mod_1_ReviewApp.swift
+//  Mod 1 Review
+//
+//  Created by Alistair Fraser on 2023-01-10.
+//
+
+import SwiftUI
+
+@main
+struct Mod_1_ReviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
