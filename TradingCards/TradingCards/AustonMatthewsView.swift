86645109266184edad8489bd4c200f544d0dfcc7
Alistair Fraser <afraser@lcs.on.ca>
2023-01-14 11:07:28 -0500

    I removed my frames and got started on the stat card

--- TradingCards/TradingCards/AustonMatthewsView.swift
+++ TradingCards/TradingCards/AustonMatthewsView.swift
@@ -11,37 +11,65 @@ struct AustonMatthewsView: View {
     var body: some View {
         VStack {
             ZStack {
-                Color(.blue)
-                    .ignoresSafeArea()
-                
-                VStack {
-                    
-                    Text("Auston Matthews")
-                        .font(.title)
-                        .bold()
-                        .foregroundColor(.white)
-                    
-                    
-                    
-                    Image("AustonMatthews")
-                        .resizable()
-                        .scaledToFit()
-                    
-                    
-                    Text("Career Stats")
-                        .foregroundColor(.white)
-                        .font(.title2)
-                        .bold()
+                Color("LeafsBlue")
 
-                }
+                    Color.white
+                    .padding()
+                    
+                Color.blue
+                    .padding(40)
+                
+                Color.white
+                    .padding(60)
+                
+                            Image("AustonMatthews")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 280, height: 280)
+    
             }
+            
+            VStack {
+                
+                HStack {
+                    
+                    VStack(alignment: .leading, spacing: 5) {
+                        
+                        Text("Career Stats")
+                            .font(.title2)
+                            .bold()
+                        Text("Games Played: 448")
+                        Text("Goals: 279")
+                        Text("Assists: 225")
+                        Text("Points: 504")
+                        Text("+/-: +95")
+                    }
+//                    .overlay(
+//                        Rectangle()
+//                            .stroke(Color.black)
+//                    )
+                    
+                    Spacer()
+                }
+//                .overlay(
+//                    Rectangle()
+//                        .stroke(Color.red)
+//                )
+                
+                Spacer()
+                
+            }
+            
         }
+        .navigationTitle("Auston Matthews")
     }
 }
 
 
 struct AustonMatthewsView_Previews: PreviewProvider {
     static var previews: some View {
-        AustonMatthewsView()
+        NavigationView {
+            AustonMatthewsView()
+        }
     }
 }
